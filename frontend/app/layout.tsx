71484019b6ef75bf6cd82ee3aa65c5cf5c9802cd
Palem Ganga Abhilash Reddy <abhipalem0836@gmail.com>
2026-09-14 11:21:02 +0530

    feat: add google site verification token

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -56,6 +56,9 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  verification: {
+    google: "SD2bK2MHpmkqEluexwbIpeLx_68Dy-AzTWj3HScTSew",
+  },
 };
 
 export default function RootLayout({
